6e94acb4fa110cd051beeb81876eca762172e8bf
Khalid <kaziz@u.northwestern.edu>
2012-11-18 21:12:58 -0600

    Added code to read a list of URL's from a file and fetch their IP addresses

--- burp-url.py
+++ burp-url.py
@@ -1,11 +1,23 @@
 import http.client
+import socket
+
+
+print(socket.gethostbyname('www.google.com'))
+
+with open("sample-urls") as urlsFile:
+	for line in urlsFile:
+		print(line.rstrip())
+		print(socket.gethostbyname(line.rstrip()))
+		
 
 httpServ = http.client.HTTPConnection("www.google.com", 80)
 httpServ.connect()
 httpServ.request('GET', "/index.html")
 
 response = httpServ.getresponse()
+
 if response.status == http.client.OK:
-	print(response.read())
+	print(response.getheaders())
+
 	
 httpServ.close()
