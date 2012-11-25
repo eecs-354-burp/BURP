c89626e24154992b188e48487de619215e647b49
Ethan Romba <ethanromba@gmail.com>
2012-11-25 15:25:53 -0600

    Added comments

--- htmlanalyzer.py
+++ htmlanalyzer.py
@@ -14,10 +14,16 @@ class HTMLAnalyzer:
   def __init__(self, html):
     self.load(html)
 
+  ##
+  # Loads an HTML string for analysis
+  ##
   def load(self, html):
     self.html = html
     self.doc = PyQuery(html)
 
+  ##
+  # Returns a dictionary with the analysis results
+  ##
   def analyze(self):
     return {
       'numChars': len( self.html ),
@@ -30,16 +36,28 @@ class HTMLAnalyzer:
       'numMetaRefresh': self.countElems('meta', self.isRefresh)
     }
 
-  def countElems(self, elem, f = None):
+  ##
+  # Counts the number of elements matching the given tag name,
+  # optionally filtered by a function
+  ##
+  def countElems(self, tagName, f = None):
     if f:
-      return len( self.doc(elem).filter(lambda i: f()) )
+      return len( self.doc(tagName).filter(lambda i: f()) )
     else:
-      return len( self.doc(elem) )
+      return len( self.doc(tagName) )
 
+  ##
+  # Returns true if the PyQuery element (this)
+  # has an "http-quiv" attribute with a value of "refresh"
+  ##
   def isRefresh(self):
     httpEquiv = PyQuery(this).attr['http-equiv']
     return ( httpEquiv and httpEquiv.find('refresh') )
 
+  ##
+  # Returns true if the PyQuery element (this)
+  # has a "src" attribute that points to a URI without a "js" extension
+  ##
   def hasWrongExtension(self):
     src = PyQuery(this).attr['src'];
     return ( src and (self.findJsExtension.search(src) == None) );
