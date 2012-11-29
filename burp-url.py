1d8705bbe7d817adffd9cf81f7b783ff62d3f8be
Khalid <kaziz@u.northwestern.edu>
2012-11-29 01:59:41 -0600

    Added basic error checking, made whois more robust

--- burp-url.py
+++ burp-url.py
@@ -2,34 +2,69 @@ import http.client
 import socket
 import whois
 import sys
+from pprint import pprint
+from urllib.parse import urlparse
 
 def getWhoIs(arg):
-  domain = whois.query('domain '+arg);
+  domain = whois.query(arg.netloc);
   return domain.__dict__;
 
 def getHttpHeaders(arg):
-  httpServ = http.client.HTTPConnection(arg, 80)
+  httpServ = http.client.HTTPConnection(arg.netloc, 80, timeout=5)
+  #httpServ.set_debuglevel(1)
   httpServ.connect()
-  httpServ.request('GET', "/")
-
+  httpServ.request('GET', arg.path)
+  
   response = httpServ.getresponse()
-  print(response.status);
   headers = response.getheaders();
   
   httpServ.close()
   
+  if (response.status == "302"):
+    return getHttpHeaders(headers["Location"])
+  
   return headers;
 
 def getIpAddr(arg):
-  return socket.gethostbyname(arg);
+  return socket.gethostbyname(arg.netloc);
+
+def parseUrl(arg):
+  url = urlparse(arg);
+  pprint(url);
+  return url;
 
 def main():
-  arg = sys.argv[1];
-  httpHeaders = getHttpHeaders(arg);
-  whois = getWhoIs(arg);
-  ip = getIpAddr(arg);
+  
+  if (len(sys.argv) != 2):
+    print("Invalid number of arguments!");
+    return None;
+    
+  #socket.setdefaulttimeout(5)
+  
+  arg = parseUrl(sys.argv[1]);
+  
+  httpHeaders = None;
+  ip = None;
+  whois = None;
+  
+  try:
+    httpHeaders = getHttpHeaders(arg);
+  except Exception as e:
+    print("unable to get HTTP headers");
+  
+  try:
+    whois = getWhoIs(arg);
+  except Exception as e:
+    print("unable to get whois")
+    
+    
+  try:
+    ip = getIpAddr(arg);
+  except Exception as e:
+    print("unable to get ip")
+  
   res = {"httpheaders" : httpHeaders, "whois" : whois, "ip" : ip};
-  print(res)
+  pprint(res)
   return res;  
 
 if __name__ == "__main__":
