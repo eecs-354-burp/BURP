8da22236070e7873732d8246793f4efd7f892a33
Ethan Romba <EthanRomba@u.northwestern.edu>
2012-11-30 04:23:26 -0600

    Implemented unknown-element percentage

--- htmlanalyzer.py
+++ htmlanalyzer.py
@@ -24,6 +24,8 @@ class HTMLAnalyzer:
 
   findUrlDomain = re.compile('^(?:http[s]?|ftp):\/\/(?:www\.)?([^:\/\s]+)')
 
+  isKnownElement = re.compile('a|abbr|acronym|address|applet|area|article|aside|audio|b|base|basefont|bdi|bdo|big|blockquote|body|br|button|canvas|caption|center|cite|code|col|colgroup|command|datalist|dd|del|details|dfn|dir|div|dl|dt|em|embed|fieldset|figcaption|figure|font|footer|form|frame|frameset|h1|h2|h3|h4|h5|h6|head|header|hgroup|hr|html|i|iframe|img|input|ins|kbd|keygen|label|legend|li|link|map|mark|menu|meta|meter|nav|nobr|noframes|noscript|object|ol|optgroup|option|output|p|param|pre|progress|q|rp|rt|ruby|s|samp|script|section|select|small|source|span|strike|strong|style|sub|summary|sup|table|tbody|td|textarea|tfoot|th|thead|time|title|tr|track|tt|u|ul|var|video|wbr')
+
   smallElementAreaThreshold = 30; # px ^ 2
 
   smallElementDimensionThreshold = 2; # px
@@ -58,6 +60,10 @@ class HTMLAnalyzer:
     numWhitespaceChars = self.countWhitespaceChars()
     numScriptChars = self.countScriptChars()
 
+    elems = self.doc('*')
+    numElements = len( elems )
+    numUnknownElements = self.countUnknownElements()
+
     unsafeUrls = self.getUnsafeIncludedUrls()
     safeUrls = self.getSafeIncludedUrls()
     externalUrls = [url for url in (unsafeUrls + safeUrls) if self.isExternalUrl(url)] 
@@ -77,7 +83,8 @@ class HTMLAnalyzer:
       'numSmallElements': self.countElems('*', self.isSmall),
       'hasDoubleDocuments': self.hasDoubleDocuments(),
       'numUnsafeIncludedUrls': len( unsafeUrls ),
-      'numExternalUrls': len( externalUrls )
+      'numExternalUrls': len( externalUrls ),
+      'percentUnknownElements': round( (numUnknownElements / numElements) * 100, 2 ),
     }
 
   ##
@@ -203,4 +210,10 @@ class HTMLAnalyzer:
   ##
   def countScriptChars(self):
     return sum(len(script) for script in self.doc('script').contents())
+
+  ##
+  # Returns the number of unknown HTML elements
+  ##
+  def countUnknownElements(self):
+    return len( [elem for elem in self.doc('*') if self.isKnownElement.match(elem.tag) == None] )
 
