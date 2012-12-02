ebf7f3b6afab31f5e6965f261ab610fedfd2c800
ChrisMoran <christophermoran2012@u.northwestern.edu>
2012-12-02 15:31:40 -0600

    incorporated others scripts

--- process_urls.py
+++ process_urls.py
@@ -5,7 +5,8 @@ import threading
 import Queue
 import codecs
 import htmlanalyzer # ethan's file in BURP-HTML
-
+import urlTokenizer # peter's file in BURP-URL
+import burp_url 
 
 logFile = codecs.open('error_log_fetcher.txt', 'a', 'utf-8')
 
@@ -13,12 +14,14 @@ class UrlInfo(dict):
     """
     UrlInfo is a dictionary with a restricted key set
     """
-    _valid_keys = set(['url', 'isBad', 'numchars','percentWhitespace', 'percentScriptChars', 'numIframes', 
-                  'numScripts', 'numScriptsWithWrongExtension', 'numEmbeds', 'numObjects', 
-                  'numHyperlinks','numMetaRefresh', 'numHiddenElements', 'numSmallElements',
-                  'hasDoubleDocuments', 'numUnsafeIncludedUrls', 'numExternalUrls', 
-                  'percentUnknownElements', 'subdomain', 'domain', 'port', 'path',
-                  'creation_date', 'last_updated', 'registrar', 'expiration_date'])
+    _valid_keys = set(['url', 'isBad', 'numCharacters','percentWhitespace', 'percentScriptContent', 
+                       'numIframes', 'numScripts', 'numScriptsWithWrongExtension', 'numEmbeds', 
+                       'numObjects', 'numHyperlinks','numMetaRefresh', 'numHiddenElements', 
+                       'numSmallElements', 'hasDoubleDocuments', 'numUnsafeIncludedUrls', 
+                       'numExternalUrls', 'percentUnknownElements', 'subdomain', 'domain', 
+                       'port', 'path', 'creation_date', 'last_updated', 'registrar', 'expiration_date',
+                       'numSuspiciousObjects', 'ip_address', 'content_type', 'expires', 'cache_control',
+                       'server','transfer_encoding'])
     def __init__(self, url, isBad):
         for key in UrlInfo._valid_keys:
             self[key] = None
@@ -29,6 +32,9 @@ class UrlInfo(dict):
         if key not in UrlInfo._valid_keys:
             raise KeyError('Invaid key')
         dict.__setitem__(self, key, value)
+
+    def is_valid_key(self, key):
+        return key in UrlInfo._valid_keys
         
     def valid_keys(self):
         return UrlInfo._valid_keys.copy()
@@ -36,6 +42,7 @@ class UrlInfo(dict):
 
 class InfoFetch(threading.Thread):
     def __init__(self, url_queue, out_queue, my_num=0):
+        threading.Thread.__init__(self)
         super(threading.Thread, self).__init__()
         self.url_queue = url_queue
         self.out_queue = out_queue
@@ -46,26 +53,57 @@ class InfoFetch(threading.Thread):
             url_pair = self.url_queue.get()
             try: #paranoia
                 url, isBad = url_pair
+                url = url.strip(',') # saw in log file a lot
                 info = UrlInfo(url, isBad)
-
                 # okay want to isolate errors as much as possible
+
                 try: # html analysis
-                    html = HTMLAnalyzer(url)
+                    html = htmlanalyzer.HTMLAnalyzer(url)
                     html_data = html.analyze()
-                    for key, value in html_data:
+                    for key, value in html_data.iteritems():
                         info[key] = value
                 except Exception as e:
-                    logFile.write('html analysis, %s, %s\n' %(url, str(e)))
+                    logFile.write('html analysis, %s, %s\n' %(url, e))
                     
+                domain = ""
                 try: # tokenizer
-                    pass
+                    tokens = urlTokenizer.get_tokens(url)
+                    info['subdomain'] = tokens[0]
+                    info['domain'] = tokens[1]
+                    domain = tokens[1]
+                    info['port'] = tokens[2]
+                    info['path'] = tokens[3]
                 except Exception as e:
-                    logFile.write('tokenizer, %s, %s\n', %(url, str(e)))
+                    logFile.write('tokenizer, %s, %s\n' % (url, str(e)))
 
-                try: # whois
-                    pass
+                
+                if domain == "":
+                    domain = url #hack
+                try: # whois and headers
+                    whois_dict = burp_url.whois_headers(url, domain)
+                    if whois_dict['httpheaders'] is not None:
+                        headers = whois_dict['httpheaders']
+                        if "Cache-Control" in headers:
+                            info['cache_control'] = headers['Cache-Control']
+                        if "Expires" in headers:
+                            info['expires'] = headers['Expires']
+                        if "Content-Type" in headers:
+                            info['content_type'] = headers['Content-Type']
+                        if "Server" in headers:
+                            info['server'] = headers['Server']
+                        if "Transfer-Encoding" in headers:
+                            info['transfer_encoding'] = headers['transfer_encoding']
+
+                    if whois_dict['whois'] is not None:
+                        whois = whois_dict['whois']
+                        for key, value in whois.iteritems():
+                            if info.is_valid_key(key):
+                                info[key] = str(value)
+
+                    if whois_dict['ip'] is not None:
+                        info['ip_address'] = whois_dict['ip']
                 except Exception as e:
-                    logFile.write('whois, %s, %s\n', % (url, str(e)))
+                    logFile.write('whois, %s, %s\n' % (url, str(e)))
 
                 self.out_queue.put(info)
             except Exception as e:
@@ -79,14 +117,15 @@ class UrlDatabaseLogger(threading.Thread):
     insertStatement = """insert into url_info (
                            url,
                            isBad,
-                           html_numchars,
+                           html_numCharacters,
                            html_percentWhitespace,
-                           html_percentScriptChars,
+                           html_percentScriptContent,
                            html_numIframes,
                            html_numScripts,
                            html_numScriptsWithWrongExtension,
                            html_numEmbeds, 
                            html_numObjects,
+                           html_numSuspiciousObjects,
                            html_numHyperlinks, 
                            html_numMetaRefresh,
                            html_numHiddenElements, 
@@ -95,6 +134,12 @@ class UrlDatabaseLogger(threading.Thread):
                            html_numUnsafeIncludedUrls,
                            html_numExternalUrls,
                            html_percentUnknownElements,
+                           ip_address,
+                           headers_content_type,
+                           headers_expires,
+                           headers_cache_control,
+                           headers_server,
+                           headers_transfer_encoding,
                            token_subdomain,
                            token_domain,
                            token_port,
@@ -105,14 +150,15 @@ class UrlDatabaseLogger(threading.Thread):
                            whois_expiration_date ) values (
                            :url, 
                            :isBad, 
-                           :numchars,
+                           :numCharacters,
                            :percentWhitespace,
-                           :percentScriptChars,
+                           :percentScriptContent,
                            :numIframes,
                            :numScripts,
                            :numScriptsWithWrongExtension,
                            :numEmbeds, 
                            :numObjects,
+                           :numSuspiciousObjects,
                            :numHyperlinks, 
                            :numMetaRefresh,
                            :numHiddenElements, 
@@ -121,6 +167,12 @@ class UrlDatabaseLogger(threading.Thread):
                            :numUnsafeIncludedUrls,
                            :numExternalUrls,
                            :percentUnknownElements,
+                           :ip_address,
+                           :content_type,
+                           :expires,
+                           :cache_control,
+                           :server,
+                           :transfer_encoding,
                            :subdomain,
                            :domain,
                            :port,
@@ -131,24 +183,23 @@ class UrlDatabaseLogger(threading.Thread):
                            :expiration_date )
     """
     def __init__(self, dbFileName, urls_queue):
-        super(threading.Thread, self).__init__() # i can never remember how to do this....
-        #threading.Thread.__init__(self)
-        self.db_conn = sqlite3.connect(dbFileName)
-        self.curs = self.db_conn.cursor()
+        threading.Thread.__init__(self)
+        self.db_file = dbFileName
         self.urls = urls_queue
 
-    def initDatabase(self):
-        createSql = '''create table url_info (
+    def init_db(self):
+        createSql = '''create table if not exists url_info (
                            url text primary key,
                            isBad boolean,
-                           html_numchars int,
+                           html_numCharacters int,
                            html_percentWhitespace int,
-                           html_percentScriptChars int,
+                           html_percentScriptContent int,
                            html_numIframes int,
                            html_numScripts int,
                            html_numScriptsWithWrongExtension int,
                            html_numEmbeds int, 
                            html_numObjects int,
+                           html_numSuspiciousObjects int,
                            html_numHyperlinks int, 
                            html_numMetaRefresh int,
                            html_numHiddenElements int, 
@@ -157,6 +208,12 @@ class UrlDatabaseLogger(threading.Thread):
                            html_numUnsafeIncludedUrls int,
                            html_numExternalUrls int,
                            html_percentUnknownElements int,
+                           ip_address text,
+                           headers_content_type text,
+                           headers_expires text,
+                           headers_cache_control text,
+                           headers_server text,
+                           headers_transfer_encoding text,
                            token_subdomain text,
                            token_domain text,
                            token_port int,
@@ -169,32 +226,40 @@ class UrlDatabaseLogger(threading.Thread):
         '''
         self.curs.execute(createSql)
 
+    def open_db(self):
+        self.db_conn = sqlite3.connect(self.db_file)
+        self.curs = self.db_conn.cursor()
+        self.init_db()
 
     def run(self):
+        self.open_db()
         while True:
             url_info = self.urls.get()
             try:
                 self.curs.execute(UrlDatabaseLogger.insertStatement, url_info)
+                self.curs.execute('COMMIT') # force db to accept changes
             except Exception as e:
                 logFile.write("error on insert %s\n" % str(e))
             self.urls.task_done()
 
     def close(self):
-        self.file.close()
+        self.db_conn.close()
 
 
 def selectBenignUrls(curs, limit=None):
     if limit is None:
-        sql = 'select url from urls where isBad=0 orery by Random()'
+        sql = 'select url from urls where isBad=0 order by Random()'
     else:
-        sql = 'select url from urls where isBad=0 orery by Random() limit %d' % limit
+        sql = 'select url from urls where isBad=0 order by Random() limit %d' % limit
     curs.execute(sql)
     return curs.fetchall()
 
-def getValidMaliciousUrls(fileName):
+def getUrlsFromFile(fileName, count=None):
     retList = []
     with codecs.open(fileName, 'r', 'utf-8') as f:
-        for line in f:
+        for c, line in enumerate(f):
+            if count is not None and c >= count:
+                break
             retList.append(line.strip())
     return retList
 
@@ -204,12 +269,12 @@ def makeUrl(maybeDomain):
         return "http://%s" % maybeDomain
     return maybeDomain
 
-# remove any previously fetched
 def getBenignUrls(dbFile, limit=None):
     conn = sqlite3.connect(dbFile)
     cursor = conn.cursor()
     urls = selectBenignUrls(cursor, limit)
     conn.close()
+    
     return [makeUrl(u[0]) for u in urls]
 
 
@@ -218,9 +283,8 @@ def getUrlsToProcess(the_good, the_bad, count=None):
     """
     returns a list of tuples (url, isBad) identifying which are malicious and benign
     """
-    malus = getValidMaliciousUrls(the_bad)
-    c = len(malus) if count is None else count
-    bene = getBenignUrls(the_good, c)
+    malus = getUrlsFromFile(the_bad, count)
+    bene = getUrlsFromFile(the_good, count)
 
     retList = []
     for good, bad in zip(bene, malus):
@@ -236,15 +300,15 @@ def fetchUrls(urls, outputDatabaseFile, threadCount=5, numUrls=None):
     url_queue = Queue.Queue()
     output_queue = Queue.Queue()
 
-    logger = UrlDatabaseLogger(outputDatabaseFile, output_queue)
-    logger.initDatabase()
-    logger.setDaemon(True)
-    logger.start()
-
+    
     for i in range(threadCount):
-        fetcher = InfoFetcher(url_queue, output_queue, i)
+        fetcher = InfoFetch(url_queue, output_queue, i)
         fetcher.setDaemon(True)
         fetcher.start()
+
+    logger = UrlDatabaseLogger(outputDatabaseFile, output_queue)
+    logger.setDaemon(True)
+    logger.start()
 
     # add numUrls urls to the queue
     for count, url in enumerate(urls):
@@ -256,18 +320,18 @@ def fetchUrls(urls, outputDatabaseFile, threadCount=5, numUrls=None):
     url_queue.join()
     output_queue.join()
 
-
 malicious_url_file = 'valid_urls'
 benign_url_db = 'urls.db'
+benign_url_file = 'benign_urls'
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         _, dbFile, numThreads = sys.argv
         
-        urls = getUrlsToProcess(benign_url_db, malicious_url_file)
+        urls = getUrlsToProcess(benign_url_file, malicious_url_file)
         fetchUrls(urls, dbFile, int(numThreads))
     else:
-        print "usage: python validate_urls.py dbFile numThreads"
+        print "usage: python process_urls.py dbFile numThreads"
 
         
 
