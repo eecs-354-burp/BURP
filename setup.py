4a86b1dc6551b9475e2398d2b649ac8ba34988e0
Ethan Romba <EthanRomba@u.northwestern.edu>
2012-12-03 22:59:30 -0600

    Changed package name to burphtml

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-  name = 'BURP-HTML',
+  name = 'burphtml',
   version = '0.1',
   packages = find_packages(),
   install_requires = ['pyquery>=1.2.4']
 )
+
