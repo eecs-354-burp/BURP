f645d538cbcb45c338a85176c72b7935606d5143
Ethan Romba <ethanromba@gmail.com>
2012-12-09 03:14:33 -0600

    Added setup script

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distribute_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup, find_packages
+
+setup(
+  name = 'BURP',
+  version = '0.1',
+  packages = find_packages(),
+  scripts = ['scripts/burp'],
+  test_suite = 'burp.html.tests',
+  install_requires = [
+    'requests>=0.14.2',
+    'pyquery>=1.2.4',
+    'liac-arff>=1.0'
+  ]
+)
