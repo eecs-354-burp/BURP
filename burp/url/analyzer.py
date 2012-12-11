18c23209e03b81d6d177642418920e6ac790ac5a
Khalid <kaziz@u.northwestern.edu>
2012-12-10 23:12:22 -0600

    Merged url analyzer and tokenizer

--- burp/url/analyzer.py
+++ burp/url/analyzer.py
@@ -1,17 +1,53 @@
 import socket
 import whois
 import sys
-from pprint import pprint
-from burp.url.tokenizer import getTokens
+from burp.url.TLD import tlds
+
+try:
+  from urllib.parse import urlsplit
+except ImportError:
+  from urlparse import urlsplit
+
 
 def getWhoIs(dom):
   """Return a dictionary of whois infomation
   Will throw exception if tld server not known, or query limit reached
   """
   ws = whois.query(dom)
-  #print(ws);
   return ws.__dict__;
 
 def getIpAddr(dom):
   """Return the ip address of the domain"""
   return socket.gethostbyname(dom);
+
+
+def getTokens(url):
+  """Returns a 4 tuple of sub-domain, domain, port, path"""
+  parsed = urlsplit(url)
+  path_and_port = parsed[1].split(':')
+  url_elements = path_and_port[0].split('.')
+  # url_elements = ["abcde","co","uk"]
+  
+  domain = ""
+  for i in range(-len(url_elements), 0):
+    last_i_elements = url_elements[i:]
+    #    i=-3: ["abcde","co","uk"]
+    #    i=-2: ["co","uk"]
+    #    i=-1: ["uk"] etc
+    
+    candidate = ".".join(last_i_elements) # abcde.co.uk, co.uk, uk
+    wildcard_candidate = ".".join(["*"] + last_i_elements[1:]) # *.co.uk, *.uk, *
+    exception_candidate = "!" + candidate
+    
+    # match tlds: 
+    if (exception_candidate in tlds):
+      domain =  ".".join(url_elements[i:]) 
+    if (candidate in tlds or wildcard_candidate in tlds):
+      domain =  ".".join(url_elements[i-1:])
+      # returns "abcde.co.uk"
+        
+  if domain == "":
+    raise ValueError("Domain not in global list of TLDs")
+
+  subdomain = path_and_port[0].replace(domain, "").strip('.')
+  return {"subdomain" : subdomain, "domain" : domain, "port" : parsed.port, "path" : parsed.path}
